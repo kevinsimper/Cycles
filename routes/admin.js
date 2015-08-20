abc2c710203042af541a27386b6927514f9fe1fb
Kevin Simper <kevin.simper@gmail.com>
2015-08-21 01:29:56 +0200

    Check the password of a user logging in

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,42 @@
+var User = require('../models/User')
+var Promise = require('bluebird')
+var bcrypt = Promise.promisifyAll(require('bcrypt'))
+
 exports.getLogin = function (req, res) {
   res.render('admin/login')
 }
 
 exports.postLogin = function (req, res) {
-  console.log(req.body)
-  res.send('ok')
+  var email = req.body.email
+  var password = req.body.password
+
+  User.findOne({
+    email: email
+  }).then(function (user) {
+    return bcrypt.compareAsync(password, user.password)
+  }).then(function (valid) {
+    if(valid) {
+      res.send('ok')
+    } else {
+      res.send('invalid')
+    }
+  }).catch(function (err) {
+    console.log(err)
+    res.sendStatus(500)
+  })
+}
+
+exports.createUser = function (req, res) {
+  var salt = bcrypt.genSaltAsync(10).then(function (salt) {
+    return bcrypt.hashAsync(req.body.password, salt)
+  }).then(function (hash) {
+    return new User({
+      email: req.body.email,
+      password: hash
+    }).save()
+  }).then(function (user) {
+    res.send('ok')
+  })
 }
 
 exports.getCompanies = function (req, res) {
